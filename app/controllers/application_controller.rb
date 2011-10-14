f677f6a099594d048062fa26ff5d0d9ce586a647
awead <amsterdamos@gmail.com>
2011-10-14 16:09:00 -0400

    DAM-99: Merged working code from pre4 release

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
+
   # Adds Hydra behaviors into the application controller
    include Hydra::Controller
   # Adds a few additional behaviors into the application controller
    include Blacklight::Controller
+
+  def layout_name
+   'hydra-head'
+  end
+
   # Please be sure to impelement current_user and user_session. Blacklight depends on
   # these methods in order to perform user specific actions.
 
@@ -13,8 +19,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def add_local_assets
-    stylesheet_links << "rockhall"
-    javascript_includes << "rockhall/rockhall.js"
+    #stylesheet_links << "rockhall"
+    #javascript_includes << "rockhall/rockhall.js"
   end
 
 end
