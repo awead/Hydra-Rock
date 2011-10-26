98ada13bcddf610b2acefc41afa9d9428dd8ac98
awead <amsterdamos@gmail.com>
2011-10-26 15:21:02 -0400

    DAM-102: mods_contributors_spec.rb needs to use equivilent-xml gem

--- spec/lib/rockhall/mods_contributors_spec.rb
+++ spec/lib/rockhall/mods_contributors_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 require "active_fedora"
 require "nokogiri"
+require "equivalent-xml"
 
 describe Rockhall::ModsContributors do
 
@@ -9,17 +10,19 @@ describe Rockhall::ModsContributors do
       include Rockhall::ModsContributors
     end
   end
-  
+
   before(:each) do
     #Fedora::Repository.stubs(:instance).returns(stub_everything())
     @contributor = ContributorTest.new
-  end  
-  
+  end
+
   describe "#person_template" do
     it "should generate a new individual node" do
       node = ContributorTest.person_template
       node.should be_kind_of(Nokogiri::XML::Element)
-      node.to_xml.should == "<name type=\"personal\" authority=\"naf\">\n  <namePart type=\"family\"/>\n  <namePart type=\"given\"/>\n  <namePart type=\"date\"/>\n  <namePart type=\"termsOfAddress\"/>\n  <role>\n    <roleTerm type=\"text\"/>\n  </role>\n</name>"
+      sample = "<name type=\"personal\" authority=\"naf\">  <namePart type=\"family\"/>  <namePart type=\"given\"/>  <namePart type=\"date\"/>  <namePart type=\"termsOfAddress\"/>  <role>    <roleTerm type=\"text\"/>  </role></name>"
+      sample_node = Nokogiri::XML(sample)
+      EquivalentXml.equivalent?(node.to_xml, sample_node, opts = { :element_order => false, :normalize_whitespace => true }).should be_true
     end
   end
 
@@ -27,16 +30,20 @@ describe Rockhall::ModsContributors do
     it "should generate a new corporate node" do
       node = ContributorTest.corporate_template
       node.should be_kind_of(Nokogiri::XML::Element)
-      node.to_xml.should == "<name type=\"corporate\" authority=\"naf\">\n  <namePart/>\n  <role>\n    <roleTerm type=\"text\"/>\n  </role>\n</name>"
+      sample = "<name type=\"corporate\" authority=\"naf\">\n  <namePart/>\n  <role>\n    <roleTerm type=\"text\"/>\n  </role>\n</name>"
+      sample_node = Nokogiri::XML(sample)
+      EquivalentXml.equivalent?(node.to_xml, sample_node, opts = { :element_order => false, :normalize_whitespace => true }).should be_true
     end
   end
-  
+
   describe "#conference_template" do
     it "should generate a new conference node" do
       node = ContributorTest.conference_template
       node.should be_kind_of(Nokogiri::XML::Element)
-      node.to_xml.should == "<name type=\"conference\" authority=\"naf\">\n  <namePart/>\n  <role>\n    <roleTerm type=\"text\"/>\n  </role>\n</name>"
+      sample = "<name type=\"conference\" authority=\"naf\">\n  <namePart/>\n  <role>\n    <roleTerm type=\"text\"/>\n  </role>\n</name>"
+      sample_node = Nokogiri::XML(sample)
+      EquivalentXml.equivalent?(node.to_xml, sample_node, opts = { :element_order => false, :normalize_whitespace => true }).should be_true
     end
   end
-  
+
 end
