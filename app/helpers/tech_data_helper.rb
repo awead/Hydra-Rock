31f13a1a9b1e7f86d3e4555d458cab7c2a9c42ae
awead <amsterdamos@gmail.com>
2011-11-11 13:08:40 -0500

    DAM-114: Displaying all the fields for ExternalVideo

---
+++ app/helpers/tech_data_helper.rb
@@ -0,0 +1,90 @@
+module TechDataHelper
+
+  def display_file_size
+    field = String.new
+    results = String.new
+    if get_values_from_datastream(@document_fedora, "descMetadata", [:size]).first.empty?
+      field << "unavailable"
+    else
+      field << get_values_from_datastream(@document_fedora, "descMetadata", [:size]).first
+      unless get_values_from_datastream(@document_fedora, "descMetadata", [:size, :units]).first.empty?
+        units = get_values_from_datastream(@document_fedora, "descMetadata", [:size, :units]).first
+        field << " " + ["(", units, ")"].join
+      end
+    end
+    results << "<dt><label for=\"video_bit_rate\">File Size</label></dt>"
+    results << "<dd class=\"field\">#{field}</dd>"
+    return results.html_safe
+
+  end
+
+
+  def display_video_bit_rate
+    field = String.new
+    results = String.new
+    if get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>0}, :bit_rate]).first.empty?
+      field << "unavailable"
+    else
+      field << get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>0}, :bit_rate]).first
+      unless get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>0}, :bit_rate, :unit]).first.empty?
+        units = get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>0}, :bit_rate, :unit]).first
+        field << " " + ["(", units, ")"].join
+      end
+    end
+    results << "<dt><label for=\"video_bit_rate\">Video Bit Rate</label></dt>"
+    results << "<dd class=\"field\">#{field}</dd>"
+    return results.html_safe
+  end
+
+  def display_video_frame_rate
+    field = String.new
+    results = String.new
+    if get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>0}, :frame_rate]).first.empty?
+      field << "unavailable"
+    else
+      value = get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>0}, :frame_rate]).first
+      field << value + " (fps)"
+    end
+    results << "<dt><label for=\"video_bit_rate\">Video Frame Rate</label></dt>"
+    results << "<dd class=\"field\">#{field}</dd>"
+    return results.html_safe
+  end
+
+  def display_audio_bit_rate
+    field = String.new
+    results = String.new
+    if get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>1}, :bit_rate]).first.empty?
+      field << "unavailable"
+    else
+      field << get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>1}, :bit_rate]).first
+      unless get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>1}, :bit_rate, :unit]).first.empty?
+        units = get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>1}, :bit_rate, :unit]).first
+        field << " " + ["(", units, ")"].join
+      end
+    end
+    results << "<dt><label for=\"audio_bit_rate\">Audio Bit Rate</label></dt>"
+    results << "<dd class=\"field\">#{field}</dd>"
+    return results.html_safe
+  end
+
+  def display_audio_sample_rate
+    field = String.new
+    results = String.new
+    if get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>1}, :sample_rate]).first.empty?
+      field << "unavailable"
+    else
+      field << get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>1}, :sample_rate]).first
+      unless get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>1}, :sample_rate, :unit]).first.empty?
+        units = get_values_from_datastream(@document_fedora, "descMetadata", [{:inst=>0}, {:essence=>1}, :sample_rate, :unit]).first
+        field << " " + ["(", units, ")"].join
+      end
+    end
+    results << "<dt><label for=\"audio_bit_rate\">Audio Sample Rate</label></dt>"
+    results << "<dd class=\"field\">#{field}</dd>"
+    return results.html_safe
+  end
+
+
+
+
+end
