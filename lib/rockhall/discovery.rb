23f68648bea5a2d786c33fbdf9ac6409803a5c8d
awead <amsterdamos@gmail.com>
2011-11-14 13:43:51 -0500

    BL-2: Digital content in Blacklight
    Archival Videos are now indexed in external Blacklight app.

--- lib/rockhall/discovery.rb
+++ lib/rockhall/discovery.rb
@@ -5,32 +5,33 @@ class Rockhall::Discovery
   include Hydra::AccessControlsEnforcement
   include Blacklight::SolrHelper
 
-  # This is a push method that queries the Solr index used by Hydra
-  # for publicly available objects and sends them to another solr index
-  # Right now, it's only doing ArchivalVideo objects
-
-  # adds objects to solr index
-  def self.get_objects
-
-    # query to return all objects in solr with discovery set to public
+  # Queries either the local Solr index used by Hydra for publicly available ArchivalVideo objects,
+  # or queries the remote discovery index for all ArchivalVideo objects
+  def self.get_objects(opts={})
     solr_params = Hash.new
     solr_params[:fl]   = "id"
-    solr_params[:q]    = "access_group_t:public AND collection_t:archival_video"
     solr_params[:qt]   = "standard"
     solr_params[:rows] = 1000
-    solr_response = Blacklight.solr.find(solr_params)
-    document_list = solr_response.docs.collect {|doc| SolrDocument.new(doc, solr_response)}
 
-    # I get:
-    #  undefined method `get_solr_response_for_doc_id' for Blacklight::SolrHelper:Module
-    # if I try this:
-    #  test = Blacklight::SolrHelper.get_solr_response_for_doc_id("rockhall:fixture_pbcore_document1")
+    if opts[:remote]
+      solr_params[:q]    = 'has_model_s:"info:fedora/afmodel:ArchivalVideo"'
+      solr = solr_connect
+      solr_response = solr.find(solr_params)
+    else
+      solr_params[:q]    = 'access_group_t:public AND has_model_s:"info:fedora/afmodel:ArchivalVideo"'
+      solr_response = Blacklight.solr.find(solr_params)
+    end
 
+    return solr_response.docs.collect {|doc| SolrDocument.new(doc, solr_response)}
+  end
+
+  # updates Blacklight index with results from get_objects
+  def self.update
     # connect to our other solr index
     solr = solr_connect
-
+    docs = get_objects
     # shove 'em in...
-    document_list.each do |doc|
+    docs.each do |doc|
       retrieve = Hash.new
       retrieve[:q]  = 'id:"' + doc.id + '"'
       retrieve[:qt] = "document"
@@ -38,27 +39,20 @@ class Rockhall::Discovery
       solr.add response["response"]["docs"].first
       solr.commit
     end
-
   end
 
-  # delete any ActiveFedora objects
-  # used prior to an update
+  # delete any ActiveFedora objects from the Blacklight index
   def self.delete_objects
     solr = solr_connect
-    solr_params = Hash.new
-    solr_params[:q]    = 'active_fedora_model_s:"ActiveFedora::Base"'
-    solr_params[:qt]   = "standard"
-    solr_params[:rows] = 1000
-    solr_response = Blacklight.solr.find(solr_params)
-
-    solr_response["response"]["docs"].each do |r|
+    docs = get_objects({:remote=>TRUE})
+    docs.each do |r|
       solr.delete_by_id r["id"]
       solr.commit
     end
   end
 
   def self.solr_connect
-    solr = RSolr.connect :url => RH_CONFIG["solr_discovery"]
+    RSolr::Ext.connect({:url => RH_CONFIG["solr_discovery"]})
   end
 
 end
---
+++ spec/lib/rockhall/discovery_spec.rb
@@ -0,0 +1,50 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Rockhall::Discovery do
+
+
+  describe "#get_objects" do
+
+    before(:all) do
+      Hydrangea::JettyCleaner.clean(RH_CONFIG["pid_space"])
+      solrizer = Solrizer::Fedora::Solrizer.new
+      solrizer.solrize_objects
+    end
+
+    after(:all) do
+      Hydrangea::JettyCleaner.clean(RH_CONFIG["pid_space"])
+      solrizer = Solrizer::Fedora::Solrizer.new
+      solrizer.solrize_objects
+      Rockhall::Discovery.delete_objects
+      Rockhall::Discovery.update
+    end
+
+    it "should delete all ArchivalVideos from the remote Blacklight index" do
+      Rockhall::Discovery.delete_objects
+      docs = Rockhall::Discovery.get_objects({:remote=>TRUE})
+      docs.should be_empty
+    end
+
+    it "should return a list of current documents" do
+      docs = Rockhall::Discovery.get_objects
+      docs.count.should == 2
+    end
+
+    it "should return an updated list of new videos" do
+      av = ArchivalVideo.new
+      av.datastreams_in_memory["rightsMetadata"].update_permissions( "group"=>{"public"=>"read"} )
+      av.save
+      docs = Rockhall::Discovery.get_objects
+      docs.count.should == 3
+    end
+
+    it "should update the Blacklight index with new videos" do
+      Rockhall::Discovery.update
+      docs = Rockhall::Discovery.get_objects({:remote=>TRUE})
+      docs.count.should == 3
+    end
+
+  end
+
+
+end
