18feb5a2da06a76ad66fa34cf922d810b9c62563
Adam Wead <amsterdamos@gmail.com>
2011-11-15 15:49:38 -0500

    rigging for production

--- config/initializers/fedora_config.rb
+++ config/initializers/fedora_config.rb
@@ -9,17 +9,17 @@ require 'active-fedora'
 silence_warnings { ENABLE_SOLR_UPDATES=true }
 
 # Don't need to call ActiveFedora.init any more.  It's handled by {ActiveFedora::Railtie}
-# ActiveFedora.init
+ActiveFedora.init
 
 #
-# Loads EAD descriptors 
+# Loads EAD descriptors
 # Attempts to parse any xml files in lib/stanford/ead/
 #
 # You can access any of the loaded descriptors using Descriptor.retrieve( id ), where "id" is the base filename of the descriptor xml file
 
 # puts "Registering EADs..."
-# Dir[File.join( RAILS_ROOT, 'lib', "stanford", "ead", "/*.xml" )].each do |f| 
+# Dir[File.join( RAILS_ROOT, 'lib', "stanford", "ead", "/*.xml" )].each do |f|
 #   puts "... EAD id #{File.basename(f, ".xml")}"
-#   Descriptor.register( File.basename(f, ".xml") ) 
+#   Descriptor.register( File.basename(f, ".xml") )
 # end
 
