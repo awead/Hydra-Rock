0724108108a7a7f2bd966c0553c5eef54a0b3742
Adam Wead <amsterdamos@gmail.com>
2011-11-29 11:38:27 -0500

    DAM-129: Fixing cucumber test warnings

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -8,27 +8,27 @@ Given /^I (?:am )?log(?:ged)? in as "([^\"]*)"$/ do |email|
   User.find_by_email(email).should_not be_nil
   visit destroy_user_session_path
   visit new_user_session_path
-  fill_in "Email", :with => email 
+  fill_in "Email", :with => email
   fill_in "Password", :with => "password"
   click_button "Sign in"
-  Then %{I should see a link to "my account info" with label "#{email}"} 
-  And %{I should see a link to "logout"} 
+  step %{I should see a link to "my account info" with label "#{email}"}
+  step %{I should see a link to "logout"}
 end
 
 Given /^I am logged in as "([^\"]*)" with "([^\"]*)" permissions$/ do |login,permission_group|
-  Given %{I am logged in as "#{login}"}
+  step %{I am logged in as "#{login}"}
   RoleMapper.roles(login).should include permission_group
 end
 
 Given /^I am a superuser$/ do
-  Given %{I am logged in as "bigwig@example.com"}
+  step %{I am logged in as "bigwig@example.com"}
   bigwig_id = User.find_by_email("bigwig@example.com").id
   superuser = Superuser.create(:id => 20, :user_id => bigwig_id)
   visit superuser_path
 end
 
 Given /^I am not logged in$/ do
-  Given %{I log out}
+  step %{I log out}
 end
 
 Given /^I log out$/ do
