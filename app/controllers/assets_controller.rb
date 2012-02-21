522deae9cad2a11bfced9f1adfcbfcfbb95e0cbb
Adam Wead <amsterdamos@gmail.com>
2012-02-21 14:31:41 -0500

    Removing debug junk

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -5,7 +5,6 @@ class AssetsController < ApplicationController
 
   def enforce_asset_creation_restrictions
     user_groups = RoleMapper.roles(current_user.login)
-    logger.info("\n\n\n\n\n#{user_groups.inspect}\n\n\n\n")
     unless user_groups.include?("archivist") or user_groups.include?("reviewer")
       flash[:notice] = "You are not allowed to create new content"
       redirect_to url_for(root_path)
