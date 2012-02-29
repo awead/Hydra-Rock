b89438e4d3ed13abd9a1a014d1e46363ac7a336e
Adam Wead <amsterdamos@gmail.com>
2012-02-29 17:01:02 -0500

    BL-167: Running blacklight and hydra generators; merged changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
+// Required by Hydra
+//= require 'jquery.hydraMetadata.js'         
+//= require 'jquery.ui.datepicker.js'      
+
+//
+// Required by Blacklight
+//= require jquery-ui
+//= require blacklight/blacklight
 //= require_tree .
--- config/initializers/hydra_config.rb
+++ config/initializers/hydra_config.rb
@@ -1,10 +1,5 @@
 require "hydra"
 
-# ActiveFedora workaround
-if Rails.env == "production"
-  ActiveFedora.init
-end
-
 # The following lines determine which user attributes your hydrangea app will use
 # This configuration allows you to use the out of the box ActiveRecord associations between users and user_attributes
 # It also allows you to specify your own user attributes
@@ -18,38 +13,33 @@ User.send(:include, Hydra::GenericUserAttributes)
 
 if Hydra.respond_to?(:configure)
   Hydra.configure(:shared) do |config|
-
+  
     # This is used as a reference by choose_model_by_filename in FileAssetsHelper
     config[:file_asset_types] = {
       # MZ -This can only be enabled if/when we adopt replacements for ImageAsset, AudioAsset, etc. as default primitives.
-      # :default => FileAsset,
+      # :default => FileAsset, 
       # :extension_mappings => {
       #   AudioAsset => [".wav", ".mp3", ".aiff"] ,
       #   VideoAsset => [".mov", ".flv", ".mp4", ".m4v"] ,
-      #   ImageAsset => [".jpeg", ".jpg", ".gif", ".png"]
+      #   ImageAsset => [".jpeg", ".jpg", ".gif", ".png"] 
       # }
     }
-
+    
     config[:submission_workflow] = {
         :mods_assets =>      [{:name => "contributor",     :edit_partial => "mods_assets/contributor_form",     :show_partial => "mods_assets/show_contributors"},
                               {:name => "publication",     :edit_partial => "mods_assets/publication_form",     :show_partial => "mods_assets/show_publication"},
                               {:name => "additional_info", :edit_partial => "mods_assets/additional_info_form", :show_partial => "mods_assets/show_additional_info"},
-                              {:name => "files",           :edit_partial => "file_assets/file_assets_form",     :show_partial => "mods_assets/show_file_assets"},
-                              {:name => "permissions",     :edit_partial => "permissions/permissions_form",     :show_partial => "mods_assets/show_permissions"}
+                              {:name => "files",           :edit_partial => "hydra/file_assets/file_assets_form",     :show_partial => "mods_assets/show_file_assets"},
+                              {:name => "permissions",     :edit_partial => "hydra/permissions/permissions_form",     :show_partial => "mods_assets/show_permissions"}
                              ],
         # Not being used right now
         :generic_contents => [{:name => "description", :edit_partial => "generic_content_objects/description_form", :show_partial => "generic_contents/show_description"},
-                              {:name => "files",       :edit_partial => "file_assets/file_assets_form",             :show_partial => "file_assets/index"},
-                              {:name => "permissions", :edit_partial => "permissions/permissions_form",             :show_partial => "generic_contents/show_permissions"},
+                              {:name => "files",       :edit_partial => "hydra/file_assets/file_assets_form",             :show_partial => "file_assets/index"},
+                              {:name => "permissions", :edit_partial => "hydra/permissions/permissions_form",             :show_partial => "generic_contents/show_permissions"},
                               {:name => "contributor", :edit_partial => "generic_content_objects/contributor_form", :show_partial => "generic_contents/show_contributors"}
-                             ],
-        # Not wired up yet
-        :archival_video   => [{:name => "document",    :edit_partial => "archival_videos/edit/_document.html.erb",  :show_partial => "archival_videos/show/_document.html.erb"},
-                              {:name => "original",    :edit_partial => "archival_videos/edit/_original.html.erb",  :show_partial => "archival_videos/show/_original.html.erb"},
-                              {:name => "permissions",     :edit_partial => "permissions/permissions_form",     :show_partial => "mods_assets/show_permissions"}
                              ]
       }
-
+    
     # This specifies the solr field names of permissions-related fields.
     # You only need to change these values if you've indexed permissions by some means other than the Hydra's built-in tooling.
     # If you change these, you must also update the permissions request handler in your solrconfig.xml to return those values
