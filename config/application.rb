f199d99dc87426a013e7f00627a5e1822311fdba
Adam Wead <amsterdamos@gmail.com>
2012-04-17 14:44:41 -0400

    minor config update

--- config/application.rb
+++ config/application.rb
@@ -51,14 +51,9 @@ module HydraRock
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true    
+    config.assets.enabled = true
     # Default SASS Configuration, check out https://github.com/rails/sass-rails for details
     config.assets.compress = !Rails.env.development?
-
-
-    # Default SASS Configuration, check out https://github.com/rails/sass-rails for details
-    config.assets.compress = !Rails.env.development?
-    config.sass.line_comments = Rails.env.development?
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
