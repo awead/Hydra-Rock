d4be626442b9fcca64fc6476865a773ad59e2eef
Adam Wead <amsterdamos@gmail.com>
2012-06-01 15:00:56 -0400

    DAM-188: Enabling edit permissions views

--- config/initializers/hydra_config.rb
+++ config/initializers/hydra_config.rb
@@ -51,7 +51,7 @@ if Hydra.respond_to?(:configure)
           {:name => "persons",     :edit_partial => "archival_videos/edit/persons",  :show_partial => "archival_videos/show/document"},
           {:name => "original",    :edit_partial => "archival_videos/edit/original", :show_partial => "archival_videos/show/original"},
           {:name => "rockhall",    :edit_partial => "archival_videos/edit/rockhall", :show_partial => "archival_videos/show/original"},
-          {:name => "permissions", :edit_partial => "permissions/permissions_form",  :show_partial => "mods_assets/show_permissions"}
+          {:name => "permissions", :edit_partial => "shared/edit/permissions",       :show_partial => "mods_assets/show_permissions"}
         ],
         :digital_videos   => [
           {:name => "titles",      :edit_partial => "digital_videos/edit/titles",     :show_partial => "digital_videos/show/document"},
