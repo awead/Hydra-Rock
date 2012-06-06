beff2b1bafc1ed2e49e950f0c88ee5719de9c497
Adam Wead <amsterdamos@gmail.com>
2012-06-05 23:35:14 -0400

    DAM-188: Digital video controller and views

--- app/models/digital_video.rb
+++ app/models/digital_video.rb
@@ -13,20 +13,50 @@ class DigitalVideo < ActiveFedora::Base
   has_relationship "objects", :is_part_of, :inbound => true
 
   has_metadata :name => "rightsMetadata", :type => Hydra::Datastream::RightsMetadata
+  has_metadata :name => "descMetadata",   :type => Rockhall::PbcoreDigitalDocument
+  has_metadata :name => "properties",     :type => Rockhall::Properties
+  has_metadata :name => "assetReview",    :type => Rockhall::AssetReview
 
-  has_metadata :name => "descMetadata", :type => Rockhall::PbcoreDigitalDocument do |m|
-  end
-
-  has_metadata :name => "properties", :type => Rockhall::Properties do |m|
-  end
-  delegate :depositor, :to=>'properties', :at=>[:depositor]
-
-  has_metadata :name => "assetReview", :type => Rockhall::AssetReview do |m|
-  end
-  delegate :reviewer,       :to=>'assetReview', :at=>[:reviewer]
-  delegate :date_updated,   :to=>'assetReview', :at=>[:date_updated]
-  delegate :complete,       :to=>'assetReview', :at=>[:complete]
-  delegate :priority,       :to=>'assetReview', :at=>[:priority]
+  delegate :reviewer,             :to=> :assetReview
+  delegate :date_updated,         :to=> :assetReview
+  delegate :complete,             :to=> :assetReview
+  delegate :priority,             :to=> :assetReview
+  delegate :license,              :to=> :assetReview
+  delegate :abstract,             :to=> :assetReview
+  delegate :main_title,           :to=> :descMetadata
+  delegate :alternative_title,    :to=> :descMetadata
+  delegate :chapter,              :to=> :descMetadata
+  delegate :episode,              :to=> :descMetadata
+  delegate :label,                :to=> :descMetadata
+  delegate :segment,              :to=> :descMetadata
+  delegate :subtitle,             :to=> :descMetadata
+  delegate :track,                :to=> :descMetadata
+  delegate :translation,          :to=> :descMetadata
+  delegate :lc_subject,           :to=> :descMetadata
+  delegate :lc_name,              :to=> :descMetadata
+  delegate :rh_subject,           :to=> :descMetadata
+  delegate :subjects,             :to=> :descMetadata
+  delegate :summary,              :to=> :descMetadata
+  delegate :parts_list,           :to=> :descMetadata
+  delegate :getty_genre,          :to=> :descMetadata
+  delegate :lc_genre,             :to=> :descMetadata
+  delegate :lc_subject_genre,     :to=> :descMetadata
+  delegate :genres,               :to=> :descMetadata
+  delegate :event_series,         :to=> :descMetadata
+  delegate :event_place,          :to=> :descMetadata
+  delegate :event_date,           :to=> :descMetadata
+  delegate :contributor_name,     :to=> :descMetadata
+  delegate :contributor_role,     :to=> :descMetadata
+  delegate :publisher_name,       :to=> :descMetadata
+  delegate :publisher_role,       :to=> :descMetadata
+  delegate :note,                 :to=> :descMetadata
+  delegate :archival_collection,  :to=> :descMetadata
+  delegate :archival_series,      :to=> :descMetadata
+  delegate :collection_number,    :to=> :descMetadata
+  delegate :accession_number,     :to=> :descMetadata
+  delegate :usage,                :to=> :descMetadata
+  delegate :depositor,            :to=> :properties
+  delegate :notes,                :to=> :properties
 
   def initialize( attrs={} )
     super
