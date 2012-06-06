2af83a620a8e66b68225eedf24336a493f502061
Adam Wead <amsterdamos@gmail.com>
2012-06-06 17:23:55 -0400

    DAM-186: New controller for external videos

---
+++ app/controllers/external_videos_controller.rb
@@ -0,0 +1,97 @@
+class ExternalVideosController < ApplicationController
+  include Blacklight::Catalog
+  include Hydra::Catalog
+  include Hydra::Controller
+  include Hydra::AssetsControllerHelper  # This is to get apply_depositor_metadata method
+
+  before_filter :authenticate_user!, :only=>[:create, :new, :edit, :update]
+  before_filter :enforce_access_controls
+  #before_filter :enforce_viewing_context_for_show_requests, :only=>[:show]
+  #before_filter :search_session, :history_session
+
+  def edit
+    @video = ExternalVideo.find(params[:id])
+    respond_to do |format|
+      format.html  { setup_next_and_previous_documents }
+      format.json  { render :json => @video }
+    end
+  end
+
+  def new
+    @video = ExternalVideo.new
+    respond_to do |format|
+      format.html  # new.html.erb
+      format.json  { render :json => @video }
+    end
+  end
+
+  def show
+    update_session
+    session[:viewing_context] = "browse"
+    @video = ExternalVideo.find(params[:id])
+    respond_to do |format|
+      format.html  { setup_next_and_previous_documents }
+      format.json  { render :json => @video }
+    end
+  end
+
+  def create
+    @video = ExternalVideo.new(params[:external_video])
+    @video.apply_depositor_metadata(current_user.login)
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to(@video, :notice => 'Post was successfully created.') }
+        format.json { render :json => @video, :status => :created, :location => @video }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @video.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    update_session
+    @video = ExternalVideo.find(params[:id])
+    changes = changed_fields(params)
+    if changes.empty?
+      redirect_to(edit_external_video_path(@video, :wf_step=>params[:wf_step]))
+    else
+      @video.update_attributes(changes)
+      logger.info("Updating these fields: #{changes.inspect}")
+      if @video.save
+        redirect_to(edit_external_video_path(@video, :wf_step=>params[:wf_step]), :notice => 'Video was updated successfully')
+      else
+        redirect_to(edit_external_video_path(@video, :wf_step=>params[:wf_step]), :notice => 'Error: Unable to save changes')
+      end
+    end
+  end
+
+  def destroy
+    af = ActiveFedora::Base.find(params[:id], :cast=>true)
+    assets = af.destroy_child_assets
+    af.delete
+    msg = "Deleted #{params[:id]}"
+    msg.concat(" and associated file_asset(s): #{assets.join(", ")}") unless assets.empty?
+    flash[:notice]= msg
+    redirect_to catalog_index_path()
+  end
+
+  def changed_fields(params)
+    changes = Hash.new
+    return changes if params[:external_video].nil?
+    video = ExternalVideo.find(params[:id])
+    params[:external_video].each do |k,v|
+      unless video.send(k.to_sym) == v or (video.send(k.to_sym).empty? and v.first.empty?) or (v.sort.uniq.count > video.send(k.to_sym).count and v.sort.uniq.first.empty?)
+        changes.store(k,v)
+      end
+    end
+    return changes
+  end
+
+  def update_session
+    logger.info "Updating session with parameters:" + params.inspect
+    session[:search][:counter] = params[:counter] unless params[:counter].nil?
+    logger.info "Session now: " + session.inspect
+  end
+
+end
