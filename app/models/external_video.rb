a56796738b9986e889ce44a6d5bbafc210fc1e47
Adam Wead <amsterdamos@gmail.com>
2012-06-07 15:51:39 -0400

    DAM-188: Adding delegate fields to External Video

--- app/models/external_video.rb
+++ app/models/external_video.rb
@@ -9,18 +9,49 @@ class ExternalVideo < ActiveFedora::Base
   has_relationship "is_member_of_collection", :has_collection_member, :inbound => true
   has_bidirectional_relationship "part_of", :is_part_of, :has_part
 
-  delegate :name,       :to=> :descMetadata
-  delegate :generation, :to=> :descMetadata
-  delegate :format,     :to=> :descMetadata
-  delegate :vendor,     :to=> :descMetadata
-  delegate :next,       :to=> :descMetadata
-  delegate :previous,   :to=> :descMetadata
-  delegate :name,       :to=> :descMetadata
-  delegate :size,       :to=> :descMetadata
-  delegate :size_units, :to=> :descMetadata
-  delegate :date,       :to=> :descMetadata
-  delegate :media_type, :to=> :descMetadata
-  delegate :colors,     :to=> :descMetadata
+  delegate :name,                    :to => :descMetadata
+  delegate :location,                :to => :descMetadata
+  delegate :date,                    :to => :descMetadata
+  delegate :generation,              :to => :descMetadata
+  delegate :media_type,              :to => :descMetadata
+  delegate :file_format,             :to => :descMetadata
+  delegate :size,                    :to => :descMetadata
+  delegate :size_units,              :to => :descMetadata
+  delegate :colors,                  :to => :descMetadata
+  delegate :duration,                :to => :descMetadata
+  delegate :rights_summary,          :to => :descMetadata
+  delegate :note,                    :to => :descMetadata
+  delegate :checksum_type,           :to => :descMetadata
+  delegate :checksum_value,          :to => :descMetadata
+  delegate :device,                  :to => :descMetadata
+  delegate :capture_soft,            :to => :descMetadata
+  delegate :trans_soft,              :to => :descMetadata
+  delegate :operator,                :to => :descMetadata
+  delegate :trans_note,              :to => :descMetadata
+  delegate :vendor,                  :to => :descMetadata
+  delegate :condition,               :to => :descMetadata
+  delegate :cleaning,                :to => :descMetadata
+  delegate :color_space,             :to => :descMetadata
+  delegate :chroma,                  :to => :descMetadata
+  delegate :standard,                :to => :descMetadata
+  delegate :language,                :to => :descMetadata
+  delegate :video_standard,          :to => :descMetadata
+  delegate :video_encoding,          :to => :descMetadata
+  delegate :video_bit_rate,          :to => :descMetadata
+  delegate :video_bit_rate_units,    :to => :descMetadata
+  delegate :frame_rate,              :to => :descMetadata
+  delegate :frame_size,              :to => :descMetadata
+  delegate :video_bit_depth,         :to => :descMetadata
+  delegate :aspect_ratio,            :to => :descMetadata
+  delegate :audio_standard,          :to => :descMetadata
+  delegate :audio_encoding,          :to => :descMetadata
+  delegate :audio_bit_rate,          :to => :descMetadata
+  delegate :audio_bit_rate_units,    :to => :descMetadata
+  delegate :audio_sample_rate,       :to => :descMetadata
+  delegate :audio_sample_rate_units, :to => :descMetadata
+  delegate :audio_bit_depth,         :to => :descMetadata
+  delegate :audio_channels,          :to => :descMetadata
+
   delegate :depositor,  :to=> :properties
 
   # Object will have either an access or a perservation datastream but not both
