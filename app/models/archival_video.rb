20977966ac35d1a6bb262e10d0b5f824cd2e395d
Adam Wead <amsterdamos@gmail.com>
2012-06-27 14:55:43 -0400

    DAM-208: Upgrade to AF 4.3

--- app/models/archival_video.rb
+++ app/models/archival_video.rb
@@ -17,56 +17,21 @@ class ArchivalVideo < ActiveFedora::Base
   has_metadata :name => "properties",     :type => Rockhall::Properties
   has_metadata :name => "assetReview",    :type => Rockhall::AssetReview
 
-  delegate :reviewer,             :to=> :assetReview
-  delegate :date_updated,         :to=> :assetReview
-  delegate :complete,             :to=> :assetReview
-  delegate :priority,             :to=> :assetReview
-  delegate :license,              :to=> :assetReview
-  delegate :abstract,             :to=> :assetReview
-  delegate :main_title,           :to=> :descMetadata
-  delegate :alternative_title,    :to=> :descMetadata
-  delegate :chapter,              :to=> :descMetadata
-  delegate :episode,              :to=> :descMetadata
-  delegate :title_label,          :to=> :descMetadata, :at=>[:label]
-  delegate :segment,              :to=> :descMetadata
-  delegate :subtitle,             :to=> :descMetadata
-  delegate :track,                :to=> :descMetadata
-  delegate :translation,          :to=> :descMetadata
-  delegate :lc_subject,           :to=> :descMetadata
-  delegate :lc_name,              :to=> :descMetadata
-  delegate :rh_subject,           :to=> :descMetadata
-  delegate :subjects,             :to=> :descMetadata
-  delegate :summary,              :to=> :descMetadata
-  delegate :parts_list,           :to=> :descMetadata
-  delegate :getty_genre,          :to=> :descMetadata
-  delegate :lc_genre,             :to=> :descMetadata
-  delegate :lc_subject_genre,     :to=> :descMetadata
-  delegate :genres,               :to=> :descMetadata
-  delegate :event_series,         :to=> :descMetadata
-  delegate :event_place,          :to=> :descMetadata
-  delegate :event_date,           :to=> :descMetadata
-  delegate :contributor_name,     :to=> :descMetadata
-  delegate :contributor_role,     :to=> :descMetadata
-  delegate :publisher_name,       :to=> :descMetadata
-  delegate :publisher_role,       :to=> :descMetadata
-  delegate :note,                 :to=> :descMetadata
-  delegate :creation_date,        :to=> :descMetadata
-  delegate :barcode,              :to=> :descMetadata
-  delegate :repository,           :to=> :descMetadata
-  delegate :format,               :to=> :descMetadata
-  delegate :standard,             :to=> :descMetadata
-  delegate :media_type,           :to=> :descMetadata
-  delegate :generation,           :to=> :descMetadata
-  delegate :language,             :to=> :descMetadata
-  delegate :colors,               :to=> :descMetadata
-  delegate :archival_collection,  :to=> :descMetadata
-  delegate :archival_series,      :to=> :descMetadata
-  delegate :collection_number,    :to=> :descMetadata
-  delegate :accession_number,     :to=> :descMetadata
-  delegate :usage,                :to=> :descMetadata
-  delegate :condition_note,       :to=> :descMetadata
-  delegate :cleaning_note,        :to=> :descMetadata
-  delegate :depositor,            :to=> :properties
-  delegate :notes,                :to=> :properties
+  delegate_to :assetReview,
+    [:reviewer, :date_updated, :complete, :priority, :license, :abstract]
+
+  delegate_to :descMetadata,
+    [:main_title, :alternative_title, :chapter, :episode, :segment, :subtitle, :track,
+     :translation, :lc_subject, :lc_name, :rh_subject, :subjects, :summary, :parts_list,
+     :getty_genre, :lc_genre, :lc_subject_genre, :genres, :event_series, :event_place,
+     :event_date, :contributor_name, :contributor_role, :publisher_name, :publisher_role,
+     :note, :creation_date, :barcode, :repository, :format, :standard, :media_type,
+     :generation, :language, :colors, :archival_collection, :archival_series,
+     :collection_number, :accession_number, :usage, :condition_note, :cleaning_note]
+
+  # label is used for the Fedora object, so we have to call our label something else
+  delegate :title_label, :to=> :descMetadata, :at=>[:label]
+
+  delegate_to :properties, [:depositor, :notes]
 
 end
--- app/models/digital_video.rb
+++ app/models/digital_video.rb
@@ -17,45 +17,19 @@ class DigitalVideo < ActiveFedora::Base
   has_metadata :name => "properties",     :type => Rockhall::Properties
   has_metadata :name => "assetReview",    :type => Rockhall::AssetReview
 
-  delegate :reviewer,             :to=> :assetReview
-  delegate :date_updated,         :to=> :assetReview
-  delegate :complete,             :to=> :assetReview
-  delegate :priority,             :to=> :assetReview
-  delegate :license,              :to=> :assetReview
-  delegate :abstract,             :to=> :assetReview
-  delegate :main_title,           :to=> :descMetadata
-  delegate :alternative_title,    :to=> :descMetadata
-  delegate :chapter,              :to=> :descMetadata
-  delegate :episode,              :to=> :descMetadata
-  delegate :title_label,          :to=> :descMetadata, :at=>[:label]
-  delegate :segment,              :to=> :descMetadata
-  delegate :subtitle,             :to=> :descMetadata
-  delegate :track,                :to=> :descMetadata
-  delegate :translation,          :to=> :descMetadata
-  delegate :lc_subject,           :to=> :descMetadata
-  delegate :lc_name,              :to=> :descMetadata
-  delegate :rh_subject,           :to=> :descMetadata
-  delegate :subjects,             :to=> :descMetadata
-  delegate :summary,              :to=> :descMetadata
-  delegate :parts_list,           :to=> :descMetadata
-  delegate :getty_genre,          :to=> :descMetadata
-  delegate :lc_genre,             :to=> :descMetadata
-  delegate :lc_subject_genre,     :to=> :descMetadata
-  delegate :genres,               :to=> :descMetadata
-  delegate :event_series,         :to=> :descMetadata
-  delegate :event_place,          :to=> :descMetadata
-  delegate :event_date,           :to=> :descMetadata
-  delegate :contributor_name,     :to=> :descMetadata
-  delegate :contributor_role,     :to=> :descMetadata
-  delegate :publisher_name,       :to=> :descMetadata
-  delegate :publisher_role,       :to=> :descMetadata
-  delegate :note,                 :to=> :descMetadata
-  delegate :archival_collection,  :to=> :descMetadata
-  delegate :archival_series,      :to=> :descMetadata
-  delegate :collection_number,    :to=> :descMetadata
-  delegate :accession_number,     :to=> :descMetadata
-  delegate :usage,                :to=> :descMetadata
-  delegate :depositor,            :to=> :properties
-  delegate :notes,                :to=> :properties
+  delegate_to :assetReview,
+    [:reviewer, :date_updated, :complete, :priority, :license, :abstract]
+
+  delegate_to :descMetadata,
+    [:main_title, :alternative_title, :chapter, :episode, :segment, :subtitle, :track,
+     :translation, :lc_subject, :lc_name, :rh_subject, :subjects, :summary, :parts_list,
+     :getty_genre, :lc_genre, :lc_subject_genre, :genres, :event_series, :event_place,
+     :event_date, :contributor_name, :contributor_role, :publisher_name, :publisher_role,
+     :note, :archival_collection, :archival_series, :collection_number, :accession_number,
+     :usage]
+
+  delegate :title_label, :to=> :descMetadata, :at=>[:label]
+
+  delegate_to :properties, [:depositor, :notes]
 
 end
