f641b1c53d71deb617353b56e6ab7e324aaa538e
Adam Wead <amsterdamos@gmail.com>
2012-07-11 13:55:05 -0400

    some explanation...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,16 @@ HydraRock::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
+  # Falling back to assets pipeline
+  #
+  # Ideally, we want all of our assets compiled so there one big, nice js and css
+  # file for everything.  Occasionally, however, some files don't get compiled for one
+  # reason or another.  In that case, asset pipeline can serve out those files.
+  # Set to "true" if you want to allow the non-compiled files to get out via
+  # asset pipeline, set to "false" if you want only the compiled stuff to be served
+  # out.  Note, not using asset pipeline in production is best (ie. setting to false),
+  # but sometimes certain css files won't compile so we'll set this to true and allow
+  # asset pipeline in production just so things will work.
   config.assets.compile = true
 
   # Generate digests for assets URLs
