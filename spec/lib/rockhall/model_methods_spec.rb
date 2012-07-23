d3cdcde7a44bb97dfc8a9e22f196d278841b0d51
Adam Wead <amsterdamos@gmail.com>
2012-07-23 17:19:57 -0400

    DAM-218: Removing test under DigitalVideo

--- spec/lib/rockhall/model_methods_spec.rb
+++ spec/lib/rockhall/model_methods_spec.rb
@@ -8,6 +8,8 @@ describe Rockhall::ModelMethods do
     it "should return a solr doc with additional fields" do
       dv = DigitalVideo.find("rockhall:fixture_pbcore_digital_document1")
       solr_doc = dv.addl_solr_fields
+      solr_doc.should be_kind_of(Hash)
+      solr_doc[:access_file_s].should be_kind_of(Array)
       solr_doc[:access_file_s].should == ["content_001_access.mp4", "content_002_access.mp4", "content_003_access.mp4"]
     end
 
