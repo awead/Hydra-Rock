eef145df40666d189e5e0c515d3b79d638cbee6b
Adam Wead <amsterdamos@gmail.com>
2012-11-14 08:10:10 -0500

    Rockhall::JettyCleaner uses RSolr

--- lib/rockhall/jetty_cleaner.rb
+++ lib/rockhall/jetty_cleaner.rb
@@ -3,15 +3,11 @@ class Rockhall::JettyCleaner
   def self.clean(pidspace=nil)
     raise "You're trying to clean out your production Fedora instance!!" if Rails.env == "production"
 
-    solr_params = Hash.new
-    solr_params[:fl]   = "id"
-    solr_params[:qt]   = "standard"
-    solr_params[:rows] = 1000
-    solr_params[:q]    = "*:*"
-    solr_response = Blacklight.solr.find(solr_params)
-    docs = solr_response.docs.collect {|doc| SolrDocument.new(doc, solr_response)}
+    # Use RSolr to query solr for all the docs
+    solr = RSolr.connect :url => Blacklight.solr.uri.to_s
+    response = solr.get 'select', :params => {:q => '*:*'}
 
-    docs.each do |doc|
+    response["response"]["docs"].each do |doc|
       namespace, number = doc.id.split(/:/)
       if namespace == pidspace or pidspace.nil?
         puts "Deleting #{doc.id}"
