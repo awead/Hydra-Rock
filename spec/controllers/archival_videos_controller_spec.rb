bd78b084cf928fb540a9953730676e518773237a
Adam Wead <amsterdamos@gmail.com>
2013-01-03 13:38:07 -0500

    New controller tests

--- spec/controllers/archival_videos_controller_spec.rb
+++ spec/controllers/archival_videos_controller_spec.rb
@@ -19,8 +19,12 @@ describe ArchivalVideosController do
 
     before :each do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
-      sign_in user
+      @user = FactoryGirl.create(:user)
+      sign_in @user
+    end
+
+    after :each do
+      sign_out @user
     end
 
     describe "new" do
@@ -31,11 +35,50 @@ describe ArchivalVideosController do
       end
     end
 
+    describe "#update" do
+
+      it "should update fields with single values" do
+        video = ArchivalVideo.new
+        video.title = "Another fake title"
+        video.save
+        changes = { 
+          :alternative_title => "Fake alt title",
+          :series => "Fake series"
+        }
+        put :update, id: video, document_fields: changes
+        video.reload
+        video.alternative_title.first.should == "Fake alt title"
+        video.series.first.should == "Fake series"
+
+        changes = { 
+          :lc_subject => ["Sub one", "Sub two", "Sub three"],
+          :rh_subject => ["RH 1", "RH 2", "RH 3"]
+        }
+        put :update, id: video, document_fields: changes
+        video.reload
+        video.subject.length.should == 6
+        video.lc_subject.should == changes[:lc_subject]
+
+        changes = { 
+          :lc_subject => ["", "Sub two", "Sub three"],
+          :alternative_title => "Fake edited alt title"
+        }
+        put :update, id: video, document_fields: changes
+        video.reload
+        video.subject.length.should == 5
+        video.alternative_title.first.should == "Fake edited alt title"
+
+      end
+
+    end
+
     describe "#create" do
+
       it "should save a new archival video" do
         post :create, :archival_video => {:title => "Fake title"}
         assert_equal 'Video was successfully created.', flash[:notice]
       end
+
     end
 
   end
