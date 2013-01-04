61b25fb1efd5cc663afc646fb689bc068a83f14f
Adam Wead <amsterdamos@gmail.com>
2013-01-04 12:19:50 -0500

    DAM-246: Controller responds to different model types

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -15,7 +15,7 @@ class NodesController < ApplicationController
   end
 
   def create
-    @object = ArchivalVideo.find(params[:archival_video_id])
+    @object = get_object
     type = params[:node].keys.first
 
     if @object.respond_to?("new_"+type)
@@ -27,13 +27,13 @@ class NodesController < ApplicationController
     if @object.errors.empty?
       @object.save
       respond_to do |format|
-        format.html { redirect_to edit_archival_video_path(params[:archival_video_id]) }
+        format.html { redirect_to edit_archival_video_path(@object.pid) }
         #format.js   { render :partial => "pbcore_nodes/edit/#{params[:node]}", :locals => {:document=>@afdoc} }
       end
     else
       flash[:notice] = "Unable to insert node: #{@object.errors.full_messages.join("<br/>")}"
       respond_to do |format|
-        format.html { redirect_to edit_archival_video_path(params[:archival_video_id]) }
+        format.html { redirect_to edit_archival_video_path(@object.pid) }
         #format.js
       end 
     end
@@ -42,5 +42,19 @@ class NodesController < ApplicationController
   def destroy
   end
 
+  protected
+
+  # Takes the first parameter ending in *_id, figures out what the model is and returns it.
+  # I don't like this, but it works...
+  def get_object(id = nil, model = nil)
+    params.keys.each do |key|
+      if key.match(/id$/)
+        parts = key.to_s.split(/_/)
+        id    = params[key]
+        model = parts[0].capitalize + parts[1].capitalize
+      end
+    end
+    eval(model).find(id)
+  end
 
 end
--- spec/controllers/nodes_controller_spec.rb
+++ spec/controllers/nodes_controller_spec.rb
@@ -4,18 +4,12 @@ describe NodesController do
 
 include Devise::TestHelpers
 
-  before :all do
-    @video = ArchivalVideo.new
-    @video.title = "Fake Title"
-    @video.save
-  end
-
   describe "when the user is not logged in" do
 
     it "should redirect me to the sign-in page" do
-      get :new, :archival_video_id => @video.pid
+      get :new, :archival_video_id => "rockhall:fixture_pbcore_document1"
       assert_redirected_to new_user_session_path
-      post :create, :archival_video_id => @video.pid
+      post :create, :archival_video_id => "rockhall:fixture_pbcore_document1"
       assert_redirected_to new_user_session_path
     end
 
@@ -33,30 +27,76 @@ include Devise::TestHelpers
       sign_out @user
     end
 
-    describe "#new" do
-      it "should render a new page" do
-        get :new, :archival_video_id => @video.pid
-        assert_response :success
+    describe "and works with an archival video" do
+
+      before :all do
+        @video = ArchivalVideo.new
+        @video.title = "Fake Title"
+        @video.save
       end
+
+      describe "#new" do
+        it "should render a new page" do
+          get :new, :archival_video_id => @video.pid
+          assert_response :success
+        end
+      end
+
+      describe "#create" do
+        it "should create a new node and add it to the existing video" do
+          post :create, :archival_video_id => @video.pid, :node => { "contributor" => {:name => "John Doe", :role => "author"}}
+          updated = ArchivalVideo.find(@video.pid)
+          updated.contributor_name.first.should == "John Doe"
+          updated.contributor_role.first.should == "author"
+        end
+
+        it "should render errors for incorrect node types" do
+          post :create, :archival_video_id => @video.pid, :node => { "foo" => {:bar => "baz"}}
+          flash[:notice].should == "Unable to insert node: Node foo is not defined"
+        end
+
+        it "should render errors for incomplete arugments" do
+          post :create, :archival_video_id => @video.pid, :node => { "contributor" => {:bar => "baz"}}
+          flash[:notice].should == "Unable to insert node: Contributor name is invalid"
+        end
+      end
+
     end
 
-    describe "#create" do
-      it "should create a new node and add it to the existing video" do
-        post :create, :archival_video_id => @video.pid, :node => { "contributor" => {:name => "John Doe", :role => "author"}}
-        updated = ArchivalVideo.find(@video.pid)
-        updated.contributor_name.first.should == "John Doe"
-        updated.contributor_role.first.should == "author"
+    describe "and works with a digial video" do
+      
+      before :all do
+        @digital = DigitalVideo.new
+        @digital.title = "Fake Title"
+        @digital.save
       end
 
-      it "should render errors for incorrect node types" do
-        post :create, :archival_video_id => @video.pid, :node => { "foo" => {:bar => "baz"}}
-        flash[:notice].should == "Unable to insert node: Node foo is not defined"
+      describe "#new" do
+        it "should render a new page" do
+          get :new, :digital_video_id => @digital.pid
+          assert_response :success
+        end
       end
 
-      it "should render errors for incomplete arugments" do
-        post :create, :archival_video_id => @video.pid, :node => { "contributor" => {:bar => "baz"}}
-        flash[:notice].should == "Unable to insert node: Contributor name is invalid"
+      describe "#create" do
+        it "should create a new node and add it to the existing video" do
+          post :create, :digital_video_id => @digital.pid, :node => { "contributor" => {:name => "John Doe", :role => "author"}}
+          updated = ArchivalVideo.find(@digital.pid)
+          updated.contributor_name.first.should == "John Doe"
+          updated.contributor_role.first.should == "author"
+        end
+
+        it "should render errors for incorrect node types" do
+          post :create, :digital_video_id => @digital.pid, :node => { "foo" => {:bar => "baz"}}
+          flash[:notice].should == "Unable to insert node: Node foo is not defined"
+        end
+
+        it "should render errors for incomplete arugments" do
+          post :create, :digital_video_id => @digital.pid, :node => { "contributor" => {:bar => "baz"}}
+          flash[:notice].should == "Unable to insert node: Contributor name is invalid"
+        end
       end
+
     end
 
   end
