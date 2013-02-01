fcdf6303edcf513c56064133c2f424201273d255
Adam Wead <amsterdamos@gmail.com>
2013-02-01 15:29:52 -0500

    Adding config notes to Selenium

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -4,3 +4,8 @@
 
 require "selenium-webdriver"
 Selenium::WebDriver::Firefox::Binary.path="/Users/adamw/Applications/Firefox.app/Contents/MacOS/firefox-bin"
+
+# If you want to run the test against the launched application
+#Capybara.run_server = false
+#Capybara.current_driver = :selenium
+#Capybara.app_host = 'http://george.rockhall.org/hydra'
