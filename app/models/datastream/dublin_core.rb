d7323407211ef65ede5ae367771028b32a850aca
Adam Wead <amsterdamos@gmail.com>
2013-03-28 22:40:29 -0400

    Updating DublinCore datastream to index into Solr

--- app/models/datastream/dublin_core.rb
+++ app/models/datastream/dublin_core.rb
@@ -2,21 +2,21 @@ class DublinCore < ActiveFedora::NokogiriDatastream
 
   set_terminology do |t|
     t.root(:path=>"dc", :namespace_prefix=>"dc")
-    t.contributor(:path=>"dc:contributor")
-    t.coverage(:path=>"dc:coverage")
-    t.creator(:path=>"dc:creator")
-    t.date(:path=>"dc:date")
-    t.description(:path=>"dc:description")
-    t.format(:path=>"dc:format")
-    t.identifier(:path=>"dc:identifier")
-    t.language(:path=>"dc:language")
-    t.publisher(:path=>"dc:publisher")
-    t.relation(:path=>"dc:relation")
-    t.rights(:path=>"dc:rights")
-    t.source(:path=>"dc:source")
-    t.subject(:path=>"dc:subject")
-    t.title(:path=>"dc:title")
-    t.type(:path=>"dc:type")
+    t.contributor(:path=>"dc:contributor", :index_as => [:searchable, :displayable])
+    t.coverage(:path=>"dc:coverage", :index_as => [:searchable, :displayable])
+    t.creator(:path=>"dc:creator", :index_as => [:searchable, :displayable])
+    t.date(:path=>"dc:date", :index_as => [:searchable, :displayable])
+    t.description(:path=>"dc:description", :index_as => [:searchable, :displayable])
+    t.format(:path=>"dc:format", :index_as => [:searchable, :displayable])
+    t.identifier(:path=>"dc:identifier", :index_as => [:searchable, :displayable])
+    t.language(:path=>"dc:language", :index_as => [:searchable, :displayable])
+    t.publisher(:path=>"dc:publisher", :index_as => [:searchable, :displayable])
+    t.relation(:path=>"dc:relation", :index_as => [:searchable, :displayable])
+    t.rights(:path=>"dc:rights", :index_as => [:searchable, :displayable])
+    t.source(:path=>"dc:source", :index_as => [:searchable, :displayable])
+    t.subject(:path=>"dc:subject", :index_as => [:searchable, :displayable])
+    t.title(:path=>"dc:title", :index_as => [:searchable, :displayable])
+    t.type(:path=>"dc:type", :index_as => [:searchable, :displayable])
   end
 
   def self.xml_template
@@ -24,6 +24,13 @@ class DublinCore < ActiveFedora::NokogiriDatastream
       xml.dc("xmlns:dc" => "http://purl.org/dc/elements/1.1/", "xmlns:dct" => "http://purl.org/dc/terms/")
     end
     return builder.doc
+  end
+
+  def to_solr(solr_doc=Solr::Document.new)
+    super(solr_doc)
+    unless self.find_by_terms(:format).nil?
+      solr_doc.merge!(:format => self.find_by_terms(:format).text)
+    end
   end
 
 end
