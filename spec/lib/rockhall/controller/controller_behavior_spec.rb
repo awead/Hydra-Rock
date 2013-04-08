a3be41c372557a035aece820c16a79548a068085
Adam Wead <amsterdamos@gmail.com>
2013-04-08 17:08:29 -0400

    Don't test DigitalVideo model

--- spec/lib/rockhall/controller/controller_behavior_spec.rb
+++ spec/lib/rockhall/controller/controller_behavior_spec.rb
@@ -16,7 +16,7 @@ describe Rockhall::Controller::ControllerBehavior do
     it "should return the name of model" do
       av = ArchivalVideo.find("rockhall:fixture_pbcore_document1")
       @controller.get_model_from_pid(av.pid).should == av
-      dv = DigitalVideo.find("rockhall:fixture_pbcore_digital_document1")
+      dv = ArchivalVideo.find("rockhall:fixture_pbcore_digital_document1")
       @controller.get_model_from_pid(dv.pid).should == dv
       ev = ExternalVideo.find("rockhall:fixture_pbcore_document3_original")
       @controller.get_model_from_pid(ev.pid).should == ev
