dcfffdfaa4cacc6be8f882d819027e31af841f68
Adam Wead <amsterdamos@gmail.com>
2013-04-08 15:53:06 -0400

    Adding spec tests for Users and Activities controllers

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe ActivitiesController do
 
+  describe "#index" do
+
+    it "should return all activities" do
+      get :index
+      assert_response :success
+      assigns(:activities).count.should == 4
+    end
+
+  end
+
 end
