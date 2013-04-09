9cf58dc67669e5a9b6b4f253182e778802b7b8f5
Adam Wead <amsterdamos@gmail.com>
2013-04-08 20:01:10 -0400

    Using archivist2 so it doesn't conflict with fixture user

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   sequence :email do |n|
     # we'll use the same email for all the tests, for now...
-    "archivist1@example.com"
+    "archivist2@example.com"
   end
 
   factory :user do
