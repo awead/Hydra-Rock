8a43fc760a40abaed34f8022cfb547db251af1e2
Adam Wead <amsterdamos@gmail.com>
2013-04-26 11:24:43 -0400

    removing DigitalVideo from nodes controller tests

--- spec/controllers/nodes_controller_spec.rb
+++ spec/controllers/nodes_controller_spec.rb
@@ -67,63 +67,6 @@ include Devise::TestHelpers
 
     end
 
-    describe "and a DigitalVideo using" do
-      
-      before :all do
-        @digital = DigitalVideo.new
-        @digital.title = "Fake Title"
-        @digital.save
-      end
-
-      describe "#new" do
-        it "should render a new page" do
-          get :new, :id => @digital.pid, :type => "contributor"
-          assert_response :success
-        end
-        it "should require a node type" do
-          get :new, :id => @digital.pid
-          flash[:notice].should == "Node type is required"
-        end
-      end
-
-      describe "#create" do
-        it "should create a new node and add it to the existing video" do
-          post :create, :id => @digital.pid, :type => "contributor", :name => "John Doe", :role => "author"
-          updated = ArchivalVideo.find(@digital.pid)
-          updated.contributor_name.first.should == "John Doe"
-          updated.contributor_role.first.should == "author"
-        end
-
-        it "should render errors for incorrect node types" do
-          post :create, :id => @digital.pid, :type => "foo", :bar => "baz"
-          flash[:notice].should == "Unable to insert node: Node foo is not defined"
-        end
-
-        it "should render errors for incomplete arugments" do
-          post :create, :id => @digital.pid, :type => "contributor", :bar => "baz"
-          flash[:notice].should == "Unable to insert node: Contributor name is invalid"
-        end
-      end
-
-      describe "#destroy" do
-
-        it "should delete a node from a given index" do
-          pending "This works when it's run individually, but not with all the other tests"
-          digital = DigitalVideo.new
-          digital.title = "Fake Title"
-          digital.save
-          digital.new_contributor({:name => "Foo"})
-          digital.save
-          digital.contributor_name.first.should == "Foo"
-          delete :destroy, :type => "contributor", :id => digital.pid, :index => 0
-          updated = DigitalVideo.find(digital.pid)
-          updated.contributor_name.first.should be_nil
-        end
-
-      end
-
-    end
-
   end
 
 end
