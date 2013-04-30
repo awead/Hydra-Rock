ab91e0cee252e611f3ce122311d09b27f3466c71
Adam Wead <amsterdamos@gmail.com>
2013-04-30 13:31:57 -0400

    DAM-283: Removing calls to .live() in rockhall.js

--- app/assets/javascripts/rockhall/rockhall.js
+++ app/assets/javascripts/rockhall/rockhall.js
@@ -18,17 +18,8 @@ function getTerms(term) {
   return items;
 };
 
-$(document).ready(showTech);
-$(document).ready(hideTech);
-$(document).ready(refreshContributors);
-$(document).ready(deleteContributors);
-$(document).ready(refreshPublishers);
-$(document).ready(deletePublishers);
-$(document).ready(refreshCollections);
-$(document).ready(deleteCollections);
 
 jQuery(document).ready(function() {
-
 
   $('.pbcGen_auto').typeahead({ source: getTerms('pbcoreGenerations') });
   $('.insPhy_auto').typeahead({ source: getTerms('instantiationPhysical') });
@@ -55,185 +46,20 @@ jQuery(document).ready(function() {
 
 });
 
-function showTech() {
-  $('.show_tech_info').live("click", function(action) {
-
-    var parent  = $(this).parent("li").attr("id");
-    $(this).toggleClass("show_tech_info");
-    $(this).toggleClass("hide_tech_info");
-    $.get(this, function(data) {
-      $("#"+parent).slideDown("normal", function() { $(this).append(data); } );
-    });
-    action.preventDefault();
-
-  });  
-}
-
-function hideTech() {
-  $('.hide_tech_info').live("click", function(action) {
-    var parent  = $(this).parent("li").attr("id");
-    $(this).toggleClass("show_tech_info");
-    $(this).toggleClass("hide_tech_info");
-    $("#"+parent+" table").slideUp("normal", function() { $(this).remove(); } );
-    action.preventDefault();
+$(document).on("click", 'a.show_tech_info', function(event) {
+  var parent  = $(this).parent("li").attr("id");
+  $(this).toggleClass("show_tech_info");
+  $(this).toggleClass("hide_tech_info");
+  $.get(this, function(data) {
+    $("#"+parent).slideDown("normal", function() { $(this).append(data); } );
   });
-}
+  event.preventDefault();
+});
 
-function refreshContributors() {
-
-  $('.refresh_contributors, .modal-backdrop').live("click", function(action) {
-    reloadContributorsForm();
-    action.preventDefault();
-  });    
-
-}
-
-function refreshPublishers() {
-
-  $('.refresh_publishers, .modal-backdrop').live("click", function(action) {
-    reloadPublishersForm();
-    action.preventDefault();
-  });    
-
-}
-
-function refreshCollections() {
-
-  $('.refresh_collections, .modal-backdrop').live("click", function(action) {
-    reloadCollectionsForm();
-    action.preventDefault();
-  });    
-
-}
-
-function deleteContributors() {
-
-  $('.delete_contributors').live("click", function(action) {
-    url = $(this).attr("href")
-    $.ajax({
-      type: "GET",
-      url: url,
-      cache: false,
-      success: reloadContributorsForm,
-      error: function(xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
-    });
-    action.preventDefault();
-  }); 
-}
-
-function deletePublishers() {
-
-  $('.delete_publishers').live("click", function(action) {
-    url = $(this).attr("href")
-    $.ajax({
-      type: "GET",
-      url: url,
-      cache: false,
-      success: reloadPublishersForm,
-      error: function(xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
-    });
-    action.preventDefault();
-  }); 
-}
-
-function deleteCollections() {
-
-  $('.delete_collections').live("click", function(action) {
-    url = $(this).attr("href")
-    $.ajax({
-      type: "GET",
-      url: url,
-      cache: false,
-      success: reloadCollectionsForm,
-      error: function(xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
-    });
-    action.preventDefault();
-  }); 
-}
-
-function reloadContributorsForm() {
-
-  // Look through our url and find the pid
-  var pid = jQuery.grep(window.location.pathname.split("/"), function (element) {
-    if(element.indexOf(":") !== -1) {
-      return element;
-    }
-  });
-
-  var url = ROOT_PATH+"nodes/"+pid+"/contributor/edit";
-
-  $.ajax({
-    type: "GET",
-    url: url,
-    cache: false,
-    success: function(data) {
-      $('#contributors_form').html(data);
-    },
-    error: function(xhr, ajaxOptions, thrownError) {
-      alert(xhr.status);
-      alert(thrownError);
-    }
-  });
-
-}
-
-function reloadPublishersForm() {
-
-  // Look through our url and find the pid
-  var pid = jQuery.grep(window.location.pathname.split("/"), function (element) {
-    if(element.indexOf(":") !== -1) {
-      return element;
-    }
-  });
-
-  var url = ROOT_PATH+"nodes/"+pid+"/publisher/edit";
-
-  $.ajax({
-    type: "GET",
-    url: url,
-    cache: false,
-    success: function(data) {
-      $('#publishers_form').html(data);
-    },
-    error: function(xhr, ajaxOptions, thrownError) {
-      alert(xhr.status);
-      alert(thrownError);
-    }
-  });
-
-}
-
-function reloadCollectionsForm() {
-
-  // Look through our url and find the pid
-  var pid = jQuery.grep(window.location.pathname.split("/"), function (element) {
-    if(element.indexOf(":") !== -1) {
-      return element;
-    }
-  });
-
-  var url = ROOT_PATH+"nodes/"+pid+"/collection/edit";
-
-  $.ajax({
-    type: "GET",
-    url: url,
-    cache: false,
-    success: function(data) {
-      $('#collections_form').html(data);
-    },
-    error: function(xhr, ajaxOptions, thrownError) {
-      alert(xhr.status);
-      alert(thrownError);
-    }
-  });
-
-}
+$(document).on("click", 'a.hide_tech_info', function(event) {
+  var parent  = $(this).parent("li").attr("id");
+  $(this).toggleClass("show_tech_info");
+  $(this).toggleClass("hide_tech_info");
+  $("#"+parent+" table").slideUp("normal", function() { $(this).remove(); } );
+  event.preventDefault();
+});
