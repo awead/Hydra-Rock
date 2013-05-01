b2c34f1000d426dc456999ac764be6464a02ee25
Adam Wead <amsterdamos@gmail.com>
2013-05-01 17:05:26 -0400

    DAM-283: Using single quotes throughout for consistency

--- app/assets/javascripts/rockhall/rockhall.js
+++ app/assets/javascripts/rockhall/rockhall.js
@@ -28,17 +28,17 @@ jQuery(document).ready(function() {
   // will be updated via the ArchivalCollections controller.
   $('.collection_select').change(function() {
     var url = ROOT_PATH
-            + "archival_collections/"
+            + 'archival_collections/'
             + $('select.collection_select option:selected').val()
-            + "/archival_components.json";
+            + '/archival_components.json';
 
     // remove existing options
     $('.component_select').empty();
 
     jQuery.getJSON(url, function(data) {
       $.each(data, function(key, val) {
-        $('.component_select').append($("<option></option>")
-          .attr("value", key).text(val));
+        $('.component_select').append($('<option></option>')
+          .attr('value', key).text(val));
       });
     });
 
@@ -46,37 +46,37 @@ jQuery(document).ready(function() {
 
 });
 
-$(document).on("click", 'a.show_tech_info', function(event) {
-  var parent  = $(this).parent("li").attr("id");
-  $(this).toggleClass("show_tech_info");
-  $(this).toggleClass("hide_tech_info");
+$(document).on('click', 'a.show_tech_info', function(event) {
+  var parent  = $(this).parent('li').attr('id');
+  $(this).toggleClass('show_tech_info');
+  $(this).toggleClass('hide_tech_info');
   $.get(this, function(data) {
-    $("#"+parent).slideDown("normal", function() { $(this).append(data); } );
+    $('#'+parent).slideDown('normal', function() { $(this).append(data); } );
   });
   event.preventDefault();
 });
 
-$(document).on("click", 'a.hide_tech_info', function(event) {
-  var parent  = $(this).parent("li").attr("id");
-  $(this).toggleClass("show_tech_info");
-  $(this).toggleClass("hide_tech_info");
-  $("#"+parent+" table").slideUp("normal", function() { $(this).remove(); } );
+$(document).on('click', 'a.hide_tech_info', function(event) {
+  var parent  = $(this).parent('li').attr('id');
+  $(this).toggleClass('show_tech_info');
+  $(this).toggleClass('hide_tech_info');
+  $('#'+parent+' table').slideUp('normal', function() { $(this).remove(); } );
   event.preventDefault();
 });
 
-$(document).on("click", '.adder', function(event) {
-  var cloneElement  = $(this).parent("div").clone();
+$(document).on('click', '.adder', function(event) {
+  var cloneElement  = $(this).parent('div').clone();
   var deleteButton  = '<button class="remover btn-danger btn-mini"><i class="icon-minus icon-white"></i></button>'
 
   // clean-up our clone
   cloneElement.find('button').replaceWith(deleteButton);
-  cloneElement.find('input').val("");
+  cloneElement.find('input').val('');
 
-  $("#"+$(this).attr("id")+"_elements").slideDown("normal", function() { $(this).append(cloneElement); } );
+  $('#'+$(this).attr('id')+'_elements').slideDown('normal', function() { $(this).append(cloneElement); } );
   event.preventDefault();
 });
 
-$(document).on("click", ".remover", function(event) {
+$(document).on('click', '.remover', function(event) {
   $(this).parent().remove();
   event.preventDefault();
 });
