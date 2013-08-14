c1fc0e269b1b791dc65221beeafb79c8d951c85a
Adam Wead <amsterdamos@gmail.com>
2013-08-14 13:41:56 -0400

    DAM-318: More-or-less complete tests of permissions from the CatalogController

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe CatalogController do
 
+  include Devise::TestHelpers
+
   it "should redirect to the home page for non-existent items" do
     pending
     get :show, :id => "bogus"
@@ -9,10 +11,67 @@ describe CatalogController do
     assert_equal "Resource id bogus was not found or is unavailable", flash[:notice] 
   end
 
-  it "should redirect to the sign-in page when accessing private content" do
-    get :show, :id => "rrhof:507"
-    assert_redirected_to new_user_session_path
-    assert_equal "You do not have sufficient access privileges to read this document, which has been marked private.", flash[:alert]
+  describe "Public users" do
+
+    it "should be granted access to public content" do
+      get :show, :id => "rockhall:fixture_pbcore_document1"
+      assert_response :success
+    end
+    it "should be denied access to private content" do
+      get :show, :id => "rrhof:507"
+      assert_redirected_to new_user_session_path
+      assert_equal "You do not have sufficient access privileges to read this document, which has been marked private.", flash[:alert]
+    end
+    
+    it "should be denied read access to disoverable content" do
+      get :show, :id => "rrhof:525"
+      assert_redirected_to new_user_session_path
+      assert_equal "You do not have sufficient access privileges to read this document, which has been marked private.", flash[:alert]
+    end
+
+  end
+
+  describe "Staff users" do
+
+    before :each do
+      @user = FactoryGirl.find_or_create(:user)
+      sign_in @user
+      User.any_instance.stubs(:groups).returns(["archivist"])
+      controller.stubs(:clear_session_user)
+    end
+
+    it "should be granted access to public content" do
+      get :show, :id => "rockhall:fixture_pbcore_document1"
+      assert_response :success
+    end
+
+    it "should have access to private content" do
+      get :show, :id => "rockhall:fixture_pbcore_document2"
+      assert_response :success
+    end
+
+  end
+
+  describe "Non-staff users" do
+
+    before :each do
+      @user = FactoryGirl.find_or_create(:user)
+      sign_in @user
+      User.any_instance.stubs(:groups).returns([])
+      controller.stubs(:clear_session_user)
+    end
+
+    it "should be granted access to public content" do
+      get :show, :id => "rockhall:fixture_pbcore_document1"
+      assert_response :success
+    end
+
+    it "should not have access to private content" do
+      get :show, :id => "rockhall:fixture_pbcore_document2"
+      assert_redirected_to root_path
+      assert_equal "You do not have sufficient access privileges to read this document, which has been marked private.", flash[:alert]
+    end
+
   end
 
 end
