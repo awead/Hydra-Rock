a589cb888c0bfe11fff78fd4afc5211796a73a3d
Adam Wead <amsterdamos@gmail.com>
2013-08-14 11:35:05 -0400

    DAM-318: Enabling default ability and permissions

--- lib/rockhall/permissions.rb
+++ lib/rockhall/permissions.rb
@@ -8,11 +8,8 @@ module Rockhall::Permissions
   end
 
   def apply_default_permissions
-    self.permissions = [
-      {:type => "group", :name => "archivist",    :access => "edit"},
-      {:type => "group", :name => "donor",        :access => "read"},
-      {:type => "user",  :name => self.depositor, :access => "edit"}
-    ]
+    self.read_groups = ["donor"]
+    self.edit_groups = ["archivist"]
     self.save
   end
 
