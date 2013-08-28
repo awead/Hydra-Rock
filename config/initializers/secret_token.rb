a308b62a59cfaf213af8a40893636f0cf9ef403a
Adam Wead <amsterdamos@gmail.com>
2013-08-28 15:12:23 -0400

    DAM-325: use .secret_key_base instead of .secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HydraRock::Application.config.secret_token = 'c8f0e3035eea282106ef1f1ed9be087cc9dfa22e2d2fcaa2a3a4bc3b316d21cb98d8be783c29883966280a99ef3e66913da2cdc7e5aeb8b7192d950462719884'
+HydraRock::Application.config.secret_key_base = "d4a04f35213b9f7aba4f91d75d9d3c18b96bb604e43be2e33f31f06903cfd3bd1a6199dbfb4e74227f550b904196c99ca9585fa3f3f94a78b667d94ed0cf1a3d"
