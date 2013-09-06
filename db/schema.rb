4b2cddf88b8b2f35807cae27d4c52be234468cac
Adam Wead <amsterdamos@gmail.com>
2013-09-06 14:10:31 -0400

    Updating schema after re-running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130829131312) do
+ActiveRecord::Schema.define(version: 20130829131322) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",     null: false
